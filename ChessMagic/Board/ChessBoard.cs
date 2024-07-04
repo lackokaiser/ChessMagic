05f7faa1df9a76b708458ceed2a655ebad6bfc99
Kaiser László <kaiser.lacko@gmail.com>
2024-07-04 11:08:08 +0200

    Added move calculation

--- ChessMagic/Board/ChessBoard.cs
+++ ChessMagic/Board/ChessBoard.cs
@@ -31,18 +31,24 @@ public class ChessBoard
     /// Finds and returns the square occupied by a king with a given color
     /// </summary>
     /// <param name="color">The color given</param>
-    /// <exception cref="ApplicationException">If the king is not on the board</exception>
-    /// <returns>The square for a king</returns>
-    public Square FindKing(PieceColor color)
+    /// <exception cref="ApplicationException">If the king is not on the board or one of the squares are invalid</exception>
+    /// <returns>The position of the king</returns>
+    public Position FindKing(PieceColor color)
     {
-        foreach (var square in _squares)
+        for (int y = 0; y < 8; y++)
         {
-            if(square.Occupant == null)
-                continue;
-            if (square.Occupant.Color == color && square.Occupant.Type == PieceType.King)
-                return square;
+            for (int x = 0; x < 8; x++)
+            {
+                Position pos = new Position(x, y);
+                Square? square = ConvertToSquare(pos);
+                if (square == null)
+                    throw new ApplicationException("invalid square");
+                if(!square.IsOccupied())
+                    continue;
+                if (square.Occupant.Color == color && square.Occupant.Type == PieceType.King)
+                    return pos;
+            }
         }
-
         throw new ApplicationException("The king should always exist on the board!");
     }
 
@@ -104,14 +110,51 @@ public class ChessBoard
         return false;
     }
 
-    public void CalculateMoves()
+    public void CalculateMoves(ChessBoard board)
     {
-        foreach (var square in _squares)
+        List<(Position, Square)> foundPoses = new();
+        
+        for (int y = 0; y < 8; y++)
         {
-            square.ThreateningSquares.Clear();
+            for (int x = 0; x < 8; x++)
+            {
+                Position pos = ConvertToPosition(x, y);
+                Square? square = ConvertToSquare(pos);
+                
+                square?.ThreateningPositions.Clear();
+                if (square == null)
+                    throw new ApplicationException("Invalid square");
+                square.GeneratePossibleMoves(pos, board);
+
+                if (square.IsOccupied())
+                {
+                    foreach (var move in square.Occupant.PossibleMoves)
+                    {
+                        Square? sqr = board.ConvertToSquare(move);
+                        if (sqr == null)
+                            throw new ApplicationException("Invalid square");
+                        board.ThreatenSquare(sqr, pos);
+                    }
+                    foundPoses.Add((pos, square));
+                }
+                    
+            }
         }
+
+        Position whiteKingPosition = FindKing(PieceColor.White);
+        Position blackKingPosition = FindKing(PieceColor.Black);
+        Square? whiteKingSquare = ConvertToSquare(whiteKingPosition);
+        Square? blackKingSquare = ConvertToSquare(blackKingPosition);
+
+        if (whiteKingSquare == null || blackKingSquare == null)
+            throw new ApplicationException("Invalid king square");
         
-        
+        foreach (var foundPose in foundPoses)
+        {
+            bool isWhite = foundPose.Item2.Occupant?.Color == PieceColor.White;
+            foundPose.Item2.FilterPossibleMoves(foundPose.Item1, this, 
+                isWhite ? whiteKingPosition : blackKingPosition, isWhite ? whiteKingSquare.ThreateningPositions : blackKingSquare.ThreateningPositions);
+        }
     }
 
     public void ThreatenSquare(Square location, Position threatenFrom)
@@ -120,7 +163,7 @@ public class ChessBoard
         
         if (threateningSquare == null) throw new ArgumentException("Threatening square is invalid");
 
-        location.ThreateningSquares.Add(threatenFrom);
+        location.ThreateningPositions.Add(threatenFrom);
     }
 
     public Piece? PerformMove(Position from, Position to, PieceColor color)
