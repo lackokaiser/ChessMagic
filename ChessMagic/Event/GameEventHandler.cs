c1a5757f89277f9e9dd7d357ffa1fb5737236cf0
Kaiser László <kaiser.lacko@gmail.com>
2024-07-06 18:15:58 +0200

    Event handler for a single square update

--- ChessMagic/Event/GameEventHandler.cs
+++ ChessMagic/Event/GameEventHandler.cs
@@ -7,6 +7,7 @@ public delegate void AlgebraicNotationUpdate(object sender, string notation);
 public delegate void NextPlayer(object sender, PieceColor color);
 public delegate void PieceMoved(object sender, PieceMovedEventArgs args);
 public delegate void GameStateChanged(object sender, GameState state);
+public delegate void SquareUpdate(object sender, Position positionUpdated);
 
 public abstract class GameEventHandler
 {
@@ -14,6 +15,7 @@ public abstract class GameEventHandler
     public event AlgebraicNotationUpdate? NotationUpdateEvent;
     public event PieceMoved? PieceMovedEvent;
     public event GameStateChanged? GameStateChangedEvent;
+    public event SquareUpdate? SquareUpdateEvent;
 
     protected void OnNotationUpdate(string notation)
     {
@@ -33,5 +35,10 @@ public abstract class GameEventHandler
     protected void OnGameStateChangedEvent(GameState state)
     {
         GameStateChangedEvent?.Invoke(this, state);
+    }
+
+    protected void OnSquareUpdateEvent(Position positionUpdated)
+    {
+        SquareUpdateEvent?.Invoke(this, positionUpdated);
     }
 }
